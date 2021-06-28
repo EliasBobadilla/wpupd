f495b09eed3463c023cda606453d919c025abe28
Elías <elias.bobadilla@gmail.com>
2021-06-27 22:43:52 -0500

    Set wupd as global commad
    - updates in readme
    - create wpupd global command

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-wallpaper-wpupd
+# Wpupd!
+
+This application downloads one image from a selected **provider** (like hallhaven) and sets it as wallpaper.
+
+The config values from search images are in **~/<user>/.config/wpupd/config.json**
+File example:
+
+    {
+       "local":"/home/saile/Pictures/wallpapers",
+       "system":"gnome",
+       "provider":"wallhaven",
+       "misc":{
+          "resolution":[
+             1600,
+             900
+          ],
+          "ratios":[
+             16,
+             9
+          ],
+          "topic":"candy",
+          "sfw":false
+       }
+    }
+
+Also, there is a **wpupd.log** file in the same directory with the app journal
--- index.js
+++ index.js
@@ -1,52 +1,7 @@
-const log = require('log4js')
-const os = require('os')
-const Wallhaven = require('./providers/wallhaven')
-const Gnome = require('./controllers/gnome')
-const Config = require('./config')
-const { download, getName } = require('./utils/image')
+const Wallpaper = require('./wallpaper')
 
-const WPUPD_LOG = '.config/wpupd/wpupd.log'
-const WPUPD_CONFIG = '.config/wpupd/config.json'
-
-log.configure({
-  appenders: {
-    log: {
-      type: 'file',
-      filename: `${os.homedir()}/${WPUPD_LOG}`,
-    },
-  },
-  categories: { default: { appenders: ['log'], level: 'ALL' } },
-})
-
-const logger = log.getLogger('default')
-
-class Wallpaper {
-  constructor() {
-    try {
-      this.config = new Config(`${os.homedir()}/${WPUPD_CONFIG}`)
-      const { local, system, provider, misc } = this.config
-      if (!local || !system || !provider) {
-        throw new Error(`You should configure "${WPUPD_CONFIG}" properly`)
-      }
-      this.provider = new Wallhaven(misc)
-      this.controller = new Gnome(misc)
-    } catch (error) {
-      logger.error(error)
-    }
-  }
-
-  async set() {
-    try {
-      const image = await this.provider.getWallpaper()
-      const path = `${this.config.local}/${getName(image)}`
-      await download(image, path)
-      await this.controller.set(path)
-      logger.info(`${path} was set as wallpaper successfully`)
-    } catch (error) {
-      logger.error(error)
-    }
-  }
+function init() {
+  new Wallpaper().set()
 }
 
-const wallpaper = new Wallpaper()
-wallpaper.set()
+module.exports = { init }

3045e2f0a995fc0ce889d1158a10e98127834a26
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-26 14:53:47 -0500

    :sparkles: Better Docs, Native Promisify Shell and Implement Create Config

--- src/utils/files.js
+++ src/utils/files.js
@@ -1,35 +1,53 @@
-const path = require('path')
-const fs = require('fs')
-const fetch = require('node-fetch')
-const os = require('os')
+const path = require("path");
+const fs = require("fs");
+const fetch = require("node-fetch");
+const os = require("os");
 
-const WPUPD_CONFIG = '.config/wpupd/config.json'
+const WPUPD_CONFIG = ".config/wpupd/config.json";
 
-function getPath (url, local) {
-  return path.join(local, path.basename(url))
+function getPath(url, local) {
+  return path.join(local, path.basename(url));
 }
 
-async function createConfig () {
+function createConfig(configPath) {
+  const defaultConfig = `
+{
+  "local": "/home/saile/pictures/wallpapers",
+  "system": "gnome",
+  "provider": "wallhaven",
+  "misc": {
+    "resolution": [1600, 900],
+    "ratios": [16, 9],
+    "topic": "code geass",
+    "sfw": false
+  }
+}
+  `;
 
+  fs.writeFileSync(configPath, defaultConfig)
 }
 
-async function getConfig () {
-  const configFile = `${os.homedir()}/${WPUPD_CONFIG}`
-  if (!fs.existsSync(configFile)) { await createConfig() }
+async function getConfig() {
+  const configFile = `${os.homedir()}/${WPUPD_CONFIG}`;
 
-  const config = await fs.promises.readFile(configFile, 'utf8')
-  const json = JSON.parse(config)
-  const { local, system, provider } = json
-  if (!local || !system || !provider) throw new Error(`The config file is wrong, check ${configFile}`)
-  return json
+  if (!fs.existsSync(configFile)) {
+    createConfig(configFile);
+  }
+
+  const config = await fs.promises.readFile(configFile, "utf8");
+  const json = JSON.parse(config);
+  const { local, system, provider } = json;
+  if (!local || !system || !provider)
+    throw new Error(`The config file is wrong, check ${configFile}`);
+  return json;
 }
 
-async function getImage (url, local) {
-  const response = await fetch(url)
-  const buffer = await response.buffer()
-  const filePath = getPath(url, local)
-  await fs.promises.writeFile(filePath, buffer)
-  return filePath
+async function getImage(url, local) {
+  const response = await fetch(url);
+  const buffer = await response.buffer();
+  const filePath = getPath(url, local);
+  await fs.promises.writeFile(filePath, buffer);
+  return filePath;
 }
 
-module.exports = { getConfig, getImage }
+module.exports = { getConfig, getImage };

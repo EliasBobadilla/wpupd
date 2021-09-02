13caf9edd8d217d6dd172d3e26bd00616704f5a8
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-09-01 21:15:16 -0500

    :sparkles: Windows Support: 4/5 Add command

--- src/utils/shell.js
+++ src/utils/shell.js
@@ -1,17 +1,20 @@
-const shell = require('util').promisify(require('child_process').exec)
+const shell = require("util").promisify(require("child_process").exec);
+const os = require("os");
 
-function getCommand (system) {
+function getCommand(system, path) {
   switch (system) {
-    case 'feh':
-      return 'feh --bg-fill'
-    case 'windows':
-      console.log('Support for Windows is a WIP.')
-      process.exit(0)
+    case "feh":
+      return `feh --bg-fill ${path}`;
+    case "windows":
+      return `reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d ${path};Start-Sleep -s 10; rundll32.exe user32.dll, UpdatePerUserSystemParameters, 0, $false`;
     default:
-      return 'gsettings set org.gnome.desktop.background picture-uri'
+      return `gsettings set org.gnome.desktop.background picture-uri ${path}`;
   }
 }
 
-const run = (system, path) => shell(`${getCommand(system)} ${path}`)
+const run = (system, path) =>
+  shell(getCommand(system, path), {
+    shell: os.platform().includes("win") ? "powershell.exe" : "/bin/bash",
+  });
 
-module.exports = run
+module.exports = run;
